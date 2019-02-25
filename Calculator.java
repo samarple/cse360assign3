95de9fa29e4d0217bc6cfaec0cd299eb535725da
samarple <samarple17@gmail.com>
2019-02-25 13:26:50 -0700

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -10,19 +10,23 @@ package cse360assign3;
 /**
  * Calculator class will add, subtract, multiply, and divide.
  * @param total	Stores the total of the calculations.
+ * @param history	Stores the history of operations
  * @author samiam
  *
  */
 public class Calculator {
 
 	private int total;
+	private String history ;
 	
 	/**
 	 * Creates Calculator object.
 	 * Stores total over course of calculations.
+	 * Creates initial history of 0.
 	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
+		history = "0" ;
 	}
 	
 	/**
@@ -34,34 +38,37 @@ public class Calculator {
 	}
 	
 	/**
-	 * Adds value to total.
+	 * Adds value to total. Adds operation to history.
 	 * @param value	The value user wants to add.
 	 * @return void
 	 */
 	public void add (int value) {
 		total += value ;
+		history += " + " + value ;
 	}
 	
 	/**
-	 * Subtracts a value from total.
+	 * Subtracts a value from total. Adds operation to history.
 	 * @param value	The value user wants to subtract.
 	 * @return void
 	 */
 	public void subtract (int value) {
 		total -= value ;
+		history += " - " + value ;
 	}
 	
 	/**
-	 * Multiplies total by a value.
+	 * Multiplies total by a value. Adds operation to history.
 	 * @param value	The value user wants to multiply by.
 	 * @return void
 	 */
 	public void multiply (int value) {
 		total *= value ;
+		history += " * " + value ;
 	}
 	
 	/**
-	 * Divides total by a value.
+	 * Divides total by a value. Adds operation to history.
 	 * @param value	The value user wants to divide by.
 	 * @return void
 	 */
@@ -70,6 +77,7 @@ public class Calculator {
 			total = 0 ;
 		else
 			total /= value ;
+		history += " / " + value ;
 	}
 	
 	/**
@@ -77,6 +85,6 @@ public class Calculator {
 	 * @return String	History of calculations.
 	 */
 	public String getHistory () {
-		return "";
+		return history;
 	}
 }
