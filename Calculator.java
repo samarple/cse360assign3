86827ec1e3ffffe9d14ef50e64370ec199beae9e
samarple <samarple17@gmail.com>
2019-02-25 11:53:10 -0700

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,79 @@
+/**
+ * Sam Marple - 470
+ * CSE 360 - Assignment 3
+ * 
+ * Practice using GitHub with Calculator class
+ */
+
+package cse360assign3;
+
+/**
+ * Calculator class will add, subtract, multiply, and divide.
+ * @param total	Stores the total of the calculations.
+ * @author samiam
+ *
+ */
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Creates Calculator object.
+	 * Stores total over course of calculations.
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Getter for total variable.
+	 * @return 0
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds value to total.
+	 * @param value	The value user wants to add.
+	 * @return void
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts a value from total.
+	 * @param value	The value user wants to subtract.
+	 * @return void
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Multiplies total by a value.
+	 * @param value	The value user wants to multiply by.
+	 * @return void
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Divides total by a value.
+	 * @param value	The value user wants to divide by.
+	 * @return void
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Gives the history of the calculations done on the Calculator object.
+	 * @return String	History of calculations.
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
